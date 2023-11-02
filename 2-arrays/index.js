42680582288954f25abd46e7a4d58e6b2a0b4cbb
madi <maditabon33@gmail.com>
2023-11-02 14:38:09 -0700

    Adding Written

---
+++ 2-arrays/index.js
@@ -0,0 +1,34 @@
+/**
+ * Arrays
+ * 
+ * always use camel case where needed
+ * 
+ * Make sure to write the question/instruction and then the answers below
+ * 
+ */
+
+/**
+ * Question 5 - concat two different arrays by using .concat() method
+ * 
+ *  
+ */
+
+var one = [1,2,3];
+var two = [4,5,6];
+console.log(one.concat(two));
+
+
+
+
+/**
+ * Question 6 - create a 2 dimensions array that is 2-by-4. Grab some values out of this array to create a sentence using concatenation.
+ * 
+ *  
+ */
+
+var numbers = [
+  ["Pizza", "Fries", "Bhicken Strips", "Burger"],
+  ["Boke", "Bepsi", "Root Beer", "Water"]
+]
+console.log("I enjoy eating " + numbers[0][2] + " and drinking " + numbers[1][1] + " for dinner.");
+
